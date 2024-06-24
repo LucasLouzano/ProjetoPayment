f69eb9f22b3cd2f878df4c2c84046966275b20de
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-24 13:49:09 -0300

    Implementação da classe PaymentValue

--- src/main/java/ProjetoPayment/Payment/model/PaymentValue.java
+++ src/main/java/ProjetoPayment/Payment/model/PaymentValue.java
@@ -1,2 +1,19 @@
-package ProjetoPayment.Payment.model;public class PaymentValue {
+package ProjetoPayment.Payment.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@Entity
+public class PaymentValue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double valor;
+    private LocalDateTime data;
+    private String snValorAtual;
 }
