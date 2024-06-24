986b7bdcbc6a7d6053de4d0e2d5d79aec579edff
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-24 13:47:16 -0300

    ImplementaÃção da classe PaymenteValueServiceImpl

--- src/main/java/ProjetoPayment/Payment/service/impl/PaymentValueServiceImpl.java
+++ src/main/java/ProjetoPayment/Payment/service/impl/PaymentValueServiceImpl.java
@@ -1,2 +1,44 @@
-package ProjetoPayment.Payment.service.impl;public class PaymentValueServiceImpl {
+package ProjetoPayment.Payment.service.impl;
+
+import ProjetoPayment.Payment.dto.PaymentValueDTO;
+import ProjetoPayment.Payment.mapper.PaymentValueMapper;
+import ProjetoPayment.Payment.model.PaymentValue;
+import ProjetoPayment.Payment.repository.PaymentValueRepository;
+import ProjetoPayment.Payment.service.PaymentValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class PaymentValueServiceImpl implements PaymentValueService {
+    @Autowired
+    private PaymentValueRepository repository;
+    private PaymentValueMapper mapper;
+    @Override
+    public PaymentValue findBySnCurrentValue(String snCurrentValue) {
+        return repository.findBySnCurrentValue(snCurrentValue).orElse(null);
+    }
+
+    @Override
+    public PaymentValueDTO save(PaymentValue paymentValue) {
+        PaymentValue valuePgm = this.findBySnCurrentValue("S");
+        if (valuePgm != null) {
+            valuePgm.setSnValorAtual("N");
+            repository.save(valuePgm);
+
+        }
+        PaymentValue newPaymentValue = new PaymentValue();
+        newPaymentValue.setValor(paymentValue.getValor());
+        newPaymentValue.setData(LocalDateTime.now());
+        newPaymentValue.setSnValorAtual("S");
+        repository.save(newPaymentValue);
+        return mapper.PagValueToPagValueDTO(newPaymentValue);
+    }
+
+    @Override
+    public List<PaymentValue> findAll() {
+        return repository.findAll();
+    }
 }
