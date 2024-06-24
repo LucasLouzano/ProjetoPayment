0e1abb4c86e5d87c74f3772b2a10b0d0b048843a
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-24 13:50:18 -0300

    Implementação da classe interface PaymentValueRepository

--- src/main/java/ProjetoPayment/Payment/repository/PaymentValueRepository.java
+++ src/main/java/ProjetoPayment/Payment/repository/PaymentValueRepository.java
@@ -1,2 +1,12 @@
-package ProjetoPayment.Payment.repository;public interface PaymentValueRepository {
+package ProjetoPayment.Payment.repository;
+
+import ProjetoPayment.Payment.model.PaymentValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaymentValueRepository extends JpaRepository<PaymentValue, Long> {
+    Optional<PaymentValue> findBySnCurrentValue(String snCurrentValue);
 }
