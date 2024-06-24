23cd4b01721b5199ffb3c05a4367bfc32e6839d5
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-24 13:53:03 -0300

    Implementação da interface mapper PaymentValueMapper

--- src/main/java/ProjetoPayment/Payment/mapper/PaymentValueMapper.java
+++ src/main/java/ProjetoPayment/Payment/mapper/PaymentValueMapper.java
@@ -1,2 +1,13 @@
-package ProjetoPayment.Payment.mapper;public interface PaymentValueMapper {
+package ProjetoPayment.Payment.mapper;
+
+import ProjetoPayment.Payment.dto.PaymentValueDTO;
+import ProjetoPayment.Payment.model.PaymentValue;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PaymentValueMapper {
+    PaymentValueMapper INSTANCE = Mappers.getMapper(PaymentValueMapper.class);
+
+    PaymentValueDTO PagValueToPagValueDTO(PaymentValue paymentValue);
 }
