cd7ee32d8e0210ee34b2e78e211f580710187e9c
LucasLouzano <lucas_louzano@hotmail.com>
2024-08-06 11:49:56 -0300

    Ajuste no paymentTest

--- src/test/java/ProjetoPayment/Payment/PaymentApplicationTests.java
+++ src/test/java/ProjetoPayment/Payment/PaymentApplicationTests.java
@@ -1,13 +1,13 @@
-package ProjetoPayment.Payment;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PaymentApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package ProjetoPayment.Payment;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PaymentApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
