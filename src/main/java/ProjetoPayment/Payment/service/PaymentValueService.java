2bc9293e1e03e82164e3ad7af1107776b8a956d6
Lucas <lucaslouzano55@gmail.com>
2025-06-18 18:57:25 -0300

    update na regra de négocio

--- src/main/java/ProjetoPayment/Payment/service/PaymentValueService.java
+++ src/main/java/ProjetoPayment/Payment/service/PaymentValueService.java
@@ -1,12 +1,14 @@
 package ProjetoPayment.Payment.service;
 
 import ProjetoPayment.Payment.dto.PaymentValueDTO;
+import ProjetoPayment.Payment.enuns.PaymentStatus;
 import ProjetoPayment.Payment.model.PaymentValue;
 
 import java.util.List;
 public interface PaymentValueService {
-    PaymentValue findBySnCurrentValue(String snCurrentValue);
     PaymentValueDTO save(PaymentValue dto);
     List<PaymentValue> findAll();
+    PaymentValue findByStatus(PaymentStatus status);
+
 
 }
