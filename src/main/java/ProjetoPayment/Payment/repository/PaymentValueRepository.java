2f3927dc5a49fd203ee2d786447ad8ff28f78ab7
Lucas <lucaslouzano55@gmail.com>
2025-06-18 18:53:36 -0300

    update flag para atualização do status

--- src/main/java/ProjetoPayment/Payment/repository/PaymentValueRepository.java
+++ src/main/java/ProjetoPayment/Payment/repository/PaymentValueRepository.java
@@ -1,5 +1,6 @@
 package ProjetoPayment.Payment.repository;
 
+import ProjetoPayment.Payment.enuns.PaymentStatus;
 import ProjetoPayment.Payment.model.PaymentValue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface PaymentValueRepository extends JpaRepository<PaymentValue, Long> {
-    Optional<PaymentValue> findBySnCurrentValue(String snCurrentValue);
+    //    Optional<PaymentValue> findBySnCurrentValue(String snCurrentValue);
+    Optional<PaymentValue> findByPaymentStatus(PaymentStatus status);
 
 }
