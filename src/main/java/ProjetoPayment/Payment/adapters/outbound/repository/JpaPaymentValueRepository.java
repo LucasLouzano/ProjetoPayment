72a92ad9cedd49fecb9091d6d505e9a4c27997e8
Lucas <lucaslouzano55@gmail.com>
2025-06-29 19:16:45 -0300

    Implementação dos adapters na interface repository JpaPaymentValueRepository

--- src/main/java/ProjetoPayment/Payment/adapters/outbound/repository/JpaPaymentValueRepository.java
+++ src/main/java/ProjetoPayment/Payment/adapters/outbound/repository/JpaPaymentValueRepository.java
@@ -1,13 +1,13 @@
-package ProjetoPayment.Payment.adapters.outbound.repositories;
+package ProjetoPayment.Payment.adapters.outbound.repository;
 
 import ProjetoPayment.Payment.adapters.outbound.entity.JpaPaymentValue;
 import ProjetoPayment.Payment.domain.model.PaymentStatus;
+import ProjetoPayment.Payment.domain.model.PaymentValue;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface JpaPaymentValueRepository extends JpaRepository<JpaPaymentValue, Long> {
     Optional<JpaPaymentValue> findByPaymentStatus(PaymentStatus status);
 
