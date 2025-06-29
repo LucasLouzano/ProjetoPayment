a1460de26f299c1dbdc484ca5ff8e01e8370d839
Lucas <lucaslouzano55@gmail.com>
2025-06-29 19:19:58 -0300

    Implementação da class PaymentValueServiceImpl

--- src/main/java/ProjetoPayment/Payment/application/PaymentValueServiceImpl.java
+++ src/main/java/ProjetoPayment/Payment/application/PaymentValueServiceImpl.java
@@ -1,12 +1,12 @@
-package ProjetoPayment.Payment.usercaser;
+package ProjetoPayment.Payment.application;
 
 import ProjetoPayment.Payment.domain.dto.PaymentValueDTO;
 import ProjetoPayment.Payment.domain.model.PaymentStatus;
+import ProjetoPayment.Payment.domain.model.PaymentValue;
+import ProjetoPayment.Payment.domain.model.PaymentValueRepository;
 import ProjetoPayment.Payment.mapper.PaymentValueMapper;
-import ProjetoPayment.Payment.adapters.outbound.repositories.JpaPaymentValueRepository;
-import ProjetoPayment.Payment.ports.input.PaymentValueUserCase;
+import ProjetoPayment.Payment.domain.userCase.PaymentValueUserCase;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,34 +14,39 @@ import java.util.List;
 
 @Service
 public class PaymentValueServiceImpl implements PaymentValueUserCase {
-    @Autowired
-    private JpaPaymentValueRepository repository;
-    @Autowired
-    private PaymentValueMapper mapper;
+    private final PaymentValueRepository repository;
+
+    private final PaymentValueMapper mapper;
+
+    public PaymentValueServiceImpl(PaymentValueRepository repository, PaymentValueMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
     @Override
-    public PaymentValueUserCase findByStatus(PaymentStatus status) {
-        return repository.findByPaymentStatus(status).orElse(null);
+    public PaymentValue findByStatus(PaymentStatus status) {
+        return repository.findByStatus(status);
     }
 
     @Transactional
     @Override
-    public PaymentValueDTO save(PaymentValueUserCase dto) {
-        PaymentValueUserCase valuePgm = this.findByStatus(PaymentStatus.CURRENT);
-        if (valuePgm != null) {
-            valuePgm.setPaymentStatus(PaymentStatus.BEFORE);
-            repository.save(valuePgm);
+    public PaymentValueDTO save(PaymentValue paymentValue) {
+        PaymentValue valueCurrent = this.findByStatus(PaymentStatus.CURRENT);
+        if (valueCurrent != null) {
+            valueCurrent.setPaymentStatus(PaymentStatus.BEFORE);
+            repository.save(valueCurrent);
         }
-        PaymentValueUserCase newPaymentValue = new PaymentValueUserCase();
+        PaymentValue newPaymentValue = new PaymentValue();
         newPaymentValue.setValor(paymentValue.getValor());
         newPaymentValue.setData(LocalDateTime.now());
         newPaymentValue.setPaymentStatus(PaymentStatus.CURRENT);
         repository.save(newPaymentValue);
-        return mapper.PagValueToPagValueDTO(newPaymentValue);
+        return mapper.toDTO(newPaymentValue);
     }
 
+
     @Override
-    public List<PaymentValueUserCase> findAll() {
+    public List<PaymentValue> findAll() {
         return repository.findAll();
     }
 
