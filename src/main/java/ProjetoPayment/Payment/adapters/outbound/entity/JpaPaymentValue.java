34f3e0f552dcdd8c1393dae04e306760f611cfb2
Lucas <lucaslouzano55@gmail.com>
2025-06-29 19:14:26 -0300

    Implementação dos adapters na class entity JpaPaymentValue

--- src/main/java/ProjetoPayment/Payment/adapters/outbound/entity/JpaPaymentValue.java
+++ src/main/java/ProjetoPayment/Payment/adapters/outbound/entity/JpaPaymentValue.java
@@ -1,2 +1,60 @@
-package ProjetoPayment.Payment.adapters.outbound.entity;public class JpaPaymentValue {
+package ProjetoPayment.Payment.adapters.outbound.entity;
+
+import ProjetoPayment.Payment.domain.model.PaymentStatus;
+import ProjetoPayment.Payment.domain.model.PaymentValue;
+import jakarta.persistence.*;
+
+
+import java.time.LocalDateTime;
+@Entity
+public class JpaPaymentValue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double valor;
+    private LocalDateTime data;
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    public JpaPaymentValue() {
+    }
+
+    public JpaPaymentValue(PaymentValue model) {
+        this.id = model.getId();
+        this.valor = model.getValor();
+        this.data = model.getData();
+        this.paymentStatus = model.getPaymentStatus();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
 }
